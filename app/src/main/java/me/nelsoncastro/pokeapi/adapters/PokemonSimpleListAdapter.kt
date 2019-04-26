f5e117402013ecb0df8993cdd3b763e7e52d7c72
Cristian Mundo <00086917@uca.edu.sv>
2019-04-25 23:19:10 -0600

    updating pokeapi

---
+++ app/src/main/java/me/nelsoncastro/pokeapi/adapters/PokemonSimpleListAdapter.kt
@@ -0,0 +1,43 @@
+package me.nelsoncastro.pokeapi.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.card_view_pokemon.view.*
+import me.nelsoncastro.pokeapi.Pojos.Pokemon
+import me.nelsoncastro.pokeapi.R
+import me.nelsoncastro.pokeapi.pokeAdapter
+
+class PokemonSimpleListAdapter(var pokemons: List<Pokemon>, val clickListener: (Pokemon) -> Unit): RecyclerView.Adapter<PokemonSimpleListAdapter.ViewHolder>(), pokeAdapter {
+    override fun changeDataSet(newDataSet: List<Pokemon>) {
+        this.pokemons =newDataSet
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonSimpleListAdapter.ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view_pokemon, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = pokemons.size
+
+    override fun onBindViewHolder(holder: PokemonSimpleListAdapter.ViewHolder, position: Int) = holder.bind(pokemons[position], clickListener)
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bind(pokemon: Pokemon, clickListener: (Pokemon) -> Unit) = with(itemView){
+
+            tv_pokemon_id.text = pokemon.id.toString()
+            tv_pokemon_nameUP.text = pokemon.name
+            tv_pokemon_type.text = pokemon.fsttype
+            tv_pokemon_name.text = pokemon.name
+            Glide.with(itemView.context)
+                .load(pokemon.sprite)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(pokemon_cv)
+            this.setOnClickListener { clickListener(pokemon) }
+        }
+    }
+
+}
---
+++ app/src/main/java/me/nelsoncastro/pokeapi/fragments/MainContentFragment.kt
@@ -0,0 +1,43 @@
+package me.nelsoncastro.pokeapi.fragments
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.list_element_pokemon.view.*
+import kotlinx.android.synthetic.main.main_content_fragment_layout.*
+import me.nelsoncastro.pokeapi.Pojos.Pokemon
+import me.nelsoncastro.pokeapi.R
+
+class MainContentFragment : Fragment(){
+
+    var pokemon = Pokemon()
+
+    companion object {
+        fun newInstance(pokemon: Pokemon): MainContentFragment{
+            val newFragment = MainContentFragment()
+            newFragment.pokemon = pokemon
+            return newFragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.main_content_fragment_layout, container, false)
+        bindData(view)
+
+        return view
+    }
+
+    fun bindData(view: View) {
+        view.tv_pokemon_name.text = pokemon.name
+        view.tv_pokemon_id.text = pokemon.id.toString()
+        view.tv_pokemon_type.text = pokemon.fsttype
+        Glide.with(view).load(pokemon.sprite)
+            .placeholder(R.drawable.ic_pokemon_go)
+            .into(tv_pokemon_img)
+    }
+
+}
